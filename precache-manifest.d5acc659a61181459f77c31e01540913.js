c2aa12de0a0af105e3df25a109550f3740ee94d2
DavidIMk <davidimmanuelmaksi@gmail.com>
2019-08-09 15:30:33 +0700

    Updates

---
+++ precache-manifest.d5acc659a61181459f77c31e01540913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80a81989f8efbcefe52c9a2e8bd958a",
+    "url": "/PokemonGO/index.html"
+  },
+  {
+    "revision": "a73d05111f45ca170556",
+    "url": "/PokemonGO/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3efbc7574a97b7a599d4",
+    "url": "/PokemonGO/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a73d05111f45ca170556",
+    "url": "/PokemonGO/static/js/2.ef65654f.chunk.js"
+  },
+  {
+    "revision": "3efbc7574a97b7a599d4",
+    "url": "/PokemonGO/static/js/main.d82fef25.chunk.js"
+  },
+  {
+    "revision": "60d18116b3f0ee5f6196",
+    "url": "/PokemonGO/static/js/runtime~main.8986ddbb.js"
+  }
+]);
